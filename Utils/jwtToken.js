a1232a4b68fb9e5e307097dd05b7ac7e87c60707
Patan AbdullaKhan <abdullakhan8999@gmail.com>
2023-09-11 12:31:21 +0530

    env and token checking  7

--- Utils/jwtToken.js
+++ Utils/jwtToken.js
@@ -11,7 +11,7 @@ const sendToken = (user, statusCode, res) => {
     ),
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: 'none',
+    sameSite: 'strict'
   };
   //response
   res.status(statusCode).cookie("token", token, options).json({
